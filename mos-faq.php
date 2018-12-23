6e39dcee5f944137978f8bad239156fc8e9a53f9
Md. Mostak Shahid <mostak.shahid@gmail.com>
2018-12-23 19:00:30 +0600

    Initial commit

---
+++ mos-faq.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Mos FAQs
+Plugin URI: http://mostak.belocal.today/plugins/mos-faq/
+Description: Mos FAQs plugin that lets you easily create, order and publicize FAQs using shortcodes.
+Version: 2.0.1
+Author: Md. Mostak Shahid
+Author URI: http://mostak.belocal.today/
+License: GPL2
+*/
+
+require_once ( plugin_dir_path( __FILE__ ) . 'mos-faq-array.php' );
+require_once ( plugin_dir_path( __FILE__ ) . 'mos-faq-functions.php' );
+require_once ( plugin_dir_path( __FILE__ ) . 'mos-faq-settings.php' );
+require_once ( plugin_dir_path( __FILE__ ) . 'mos-faq-post-types.php' );
+require_once ( plugin_dir_path( __FILE__ ) . 'mos-faq-taxonomy.php' );
